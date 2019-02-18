d355c8f1033af1553de36843e9706774d6763761
Marta Paz <pazmarta@gmail.com>
2019-02-18 12:46:43 +0000

    Write and read from a list and display the message to a user - add h1 tags

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def index():
 @app.route("/<username>")
 def user(username):
     """ Display chat messages """
-    return "Welcome {0} - {1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
