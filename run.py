bfdbaaf6a774cea2d39cd602f4386634f796a025
Marta Paz <pazmarta@gmail.com>
2019-02-18 12:30:19 +0000

    Store the messages - python list

--- run.py
+++ run.py
@@ -2,19 +2,26 @@ import os # we'll have access to the environment variables
 from flask import Flask
 
 app = Flask(__name__) # initialize Flask app
+messages = [] # create an empty list
+
+def add_messages(username, message):
+    """ It takes our user name and message and append it to the list """
+    messages.append("{}: {}".format(username, message))
 
 # create app route decorator
 @app.route("/")
 def index():
-    """Main page with instructions"""
+    """ Main page with instructions """
     return "To send a message use: /USERNAME/MESSAGE"
 
 @app.route("/<username>")
 def user(username):
-    return "Hi " + username
+    """ Display chat messages """
+    return "Welcome {0}".format(username, messages)
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
+    """ Create a new message and redirect back to the chat page"""
     return "{0}: {1}".format(username, message)
     
 # app.run
